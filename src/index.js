9955e9ea9cf508ce293d6a78642c3d47e52cdf1c
Fernan de Dios <info@mediagramstudios.com>
2018-03-19 12:51:21 +0800

    added encryptContent method

--- src/index.js
+++ src/index.js
@@ -20,7 +20,28 @@ const easyJose = (kty = 'RSA', size = 2048) => {
         }
       }
 
+      // return both public and private keys
       return { public_key, private_key: result.toJSON(true) };
+    }
+    catch (err) throw new Error(err);
+  }
+
+  async function encryptContent(public_key, payload, isEncoded) {
+    if (!payload) throw new Error('Missing encrypted payload');
+    
+    let key = public_key;
+    // is public key base64 encoded
+    if (isEncoded) {
+      key = JSON.parse(URLSafeBase64.decode(public_key).toString('utf8'));
+    }
+
+    try {
+      const prepare = await jose.JWK.asKey(key);
+      const data = await jose.JWE.createEncrypt(key)
+        .update(JSON.stringify(payload))
+        .final();
+
+      return data;
     }
     catch (err) throw new Error(err);
   }
